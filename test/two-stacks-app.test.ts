aa03d62939ac5525dd71d7a89a282b9476ed7096
pabcrudel <pabcrudel@gmail.com>
2023-08-18 09:26:52 +0200

    Initial commit

---
+++ test/two-stacks-app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TwoStacksApp from '../lib/two-stacks-app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/two-stacks-app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TwoStacksApp.TwoStacksAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
